e871b41d3d7efd89cb41dbbd0e05880dae046411
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-14 15:00:56 +0000

    This is my code in php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html lang="en-ca">
+  <head>
+    <!-- Metadata -->
+    <meta charset="utf-8">
+    <meta name="description" content="user input">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="Shem Irekpita">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title>Volume of a sphere </title>
+  </head>
+  <body>
+		<!-- php echo to print the html to the page -->
+    <?php 
+			echo "<h3>Volume of a sphere</h3>";
+		?>
+		<!-- form to get the base and height from the user -->
+    <form action="./result.php" method="post" target="results">
+        <label for="Radius">Radius</label>
+        <input type="text" id="Radius" placeholder="Enter Radius" name="Radius">
+		</form>
+
+			<iframe id="results" name="results">			
+   <div id="user-info">
+      <div id="$Volume"></div>
+</div>
+	    </iframe>
+	</body>
+	</body>
+</html>
---
+++ result.php
@@ -0,0 +1,6 @@
+<?php
+	$Radius = $_POST['Radius'];
+	$Volume =(4/3) * radius**3 *pi()
+?>
+<h3>Results:</h3>
+Volume of a sphere is <?php echo "$Volume" ?>cm<sup>2</sup>.

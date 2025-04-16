d0f0b9438b953b966966eef2779c2e262ddffc2a
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-16 14:56:07 +0000

    This is my project

--- result.php
+++ result.php
@@ -1,6 +1,6 @@
 <?php
 	$radius = $_POST['Radius'];
-	$volume =(4/3) * $radius**3 *pi().(round($volume,2));
+	$volume =(4/3) * $radius**3 *pi();
 ?>
 <h3>Results:</h3>
-Volume of a sphere is <?php echo "$volume" ?>cm<sup>3</sup>.
+Volume of a sphere is <?php echo "$volume" round($volume,2) ?>cm<sup>3</sup>.
